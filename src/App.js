238dbd69074de8df2ed66b81510168b6d077ce22
Jugal Senjaliya <senjaliyajugal@gmail.com>
2021-07-01 19:28:14 +0530

    1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import AddIcon from '@material-ui/icons/Add'
 
 function App() {
   return (
-    <div>Hello</div>
+    <div className="wrapper">
+      <div className="header">
+        <h1>Google Keep</h1>
+      </div>
+
+      <div className="input_div">
+        <input type="text" placeholder="Title..." autoComplete="off" />
+        <textarea placeholder="Enter Your Note..."></textarea>
+        <button type="button"><AddIcon/></button>
+      </div>
+    </div>
   );
 }
 
